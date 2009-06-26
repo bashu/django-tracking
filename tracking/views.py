39c3ced79f0be65456f1fbfe526e985bd8e4a11b
codekoala <none@none>
2009-06-26 15:57:49 +0000

    issue #8 - made it possible to easily override the template that the visitor_map.html template extends or to change the template that is used to render the map altogether.
    --HG--
    extra : convert_revision : svn%3A26162cb9-6354-0410-9f28-5df552e62f89/trunk%4036

--- tracking/views.py
+++ tracking/views.py
@@ -6,6 +6,9 @@ from django.conf import settings
 from tracking.models import Visitor, u_clean as uc
 from datetime import datetime
 
+DEFAULT_TRACKING_TEMPLATE = getattr(settings, 'DEFAULT_TRACKING_TEMPLATE',
+                                    'tracking/visitor_map.html')
+
 def update_active_users(request):
     """
     Returns a list of all active users
@@ -77,13 +80,15 @@ def friendly_time(last_update):
 
     return friendly_time
 
-def display_map(request):
+def display_map(request, template_name=DEFAULT_TRACKING_TEMPLATE, 
+        extends_template='base.html'):
     """
     Displays a map of recently active users.  Requires a Google Maps API key
     and GeoIP in order to be most effective.
     """
     GOOGLE_MAPS_KEY = getattr(settings, 'GOOGLE_MAPS_KEY', None)
 
-    return render_to_response('tracking/visitor_map.html',
-                              {'GOOGLE_MAPS_KEY': GOOGLE_MAPS_KEY},
+    return render_to_response(template_name,
+                              {'GOOGLE_MAPS_KEY': GOOGLE_MAPS_KEY,
+                               'template': extends_template},
                               context_instance=RequestContext(request))
