46633cc1006e0c410b82f7f4d6434c76a8c8967d
Jannis Leidel <jannis@leidel.info>
2010-03-24 12:11:50 +0100

    Fixed package data handling for Mac OS X and other platforms.

--- setup.py
+++ setup.py
@@ -2,8 +2,28 @@
 # -*- coding: utf-8 -*-
 
 from distutils.core import setup
+from distutils.command.install_data import install_data
+from distutils.command.install import INSTALL_SCHEMES
 import sys, os
 import tracking
+
+class osx_install_data(install_data):
+    # On MacOS, the platform-specific lib dir is /System/Library/Framework/Python/.../
+    # which is wrong. Python 2.5 supplied with MacOS 10.5 has an Apple-specific fix
+    # for this in distutils.command.install_data#306. It fixes install_lib but not
+    # install_data, which is why we roll our own install_data class.
+
+    def finalize_options(self):
+        # By the time finalize_options is called, install.install_lib is set to the
+        # fixed directory, so we set the installdir to install_lib. The
+        # install_data class uses ('install_data', 'install_dir') instead.
+        self.set_undefined_options('install', ('install_lib', 'install_dir'))
+        install_data.finalize_options(self)
+
+if sys.platform == "darwin":
+    cmdclasses = {'install_data': osx_install_data}
+else:
+    cmdclasses = {'install_data': install_data}
 
 def fullsplit(path, result=None):
     """
@@ -18,6 +38,12 @@ def fullsplit(path, result=None):
     if head == path:
         return result
     return fullsplit(head, [tail] + result)
+
+# Tell distutils to put the data_files in platform-specific installation
+# locations. See here for an explanation:
+# http://groups.google.com/group/comp.lang.python/browse_thread/thread/35ec7b2fed36eaec/2105ee4d9e8042cb
+for scheme in INSTALL_SCHEMES.values():
+    scheme['data'] = scheme['purelib']
 
 packages, data_files = [], []
 root_dir = os.path.dirname(__file__)
@@ -43,6 +69,7 @@ setup(
     author_email='codekoala@gmail.com',
     license='MIT',
     packages=packages,
+    cmdclass = cmdclasses,
     data_files=data_files,
     description="Basic visitor tracking and blacklisting for Django",
     long_description="""
