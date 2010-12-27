50072d3d61e6860866b5770760f906e4dea0bf0a
Josh VanderLinden <codekoala@gmail.com>
2010-12-27 03:32:56 -0500

    Handling some problems in the setup script...

--- tracking/listeners.py
+++ tracking/listeners.py
@@ -1,29 +1,34 @@
 import logging
 
-from django.core.cache import cache
-from django.db.models.signals import post_save, post_delete
-from tracking.models import UntrackedUserAgent, BannedIP
-
 log = logging.getLogger('tracking.listeners')
 
-def refresh_untracked_user_agents(sender, instance, created=False, **kwargs):
-    """Updates the cache of user agents that we don't track"""
+try:
+    from django.core.cache import cache
+    from django.db.models.signals import post_save, post_delete
 
-    log.debug('Updating untracked user agents cache')
-    cache.set('_tracking_untracked_uas',
-        UntrackedUserAgent.objects.all(),
-        3600)
+    from tracking.models import UntrackedUserAgent, BannedIP
+except ImportError:
+    pass
+else:
 
-def refresh_banned_ips(sender, instance, created=False, **kwargs):
-    """Updates the cache of banned IP addresses"""
+    def refresh_untracked_user_agents(sender, instance, created=False, **kwargs):
+        """Updates the cache of user agents that we don't track"""
 
-    log.debug('Updating banned IP cache')
-    cache.set('_tracking_banned_ips',
-        [b.ip_address for b in BannedIP.objects.all()],
-        3600)
+        log.debug('Updating untracked user agents cache')
+        cache.set('_tracking_untracked_uas',
+            UntrackedUserAgent.objects.all(),
+            3600)
 
-post_save.connect(refresh_untracked_user_agents, sender=UntrackedUserAgent)
-post_delete.connect(refresh_untracked_user_agents, sender=UntrackedUserAgent)
+    def refresh_banned_ips(sender, instance, created=False, **kwargs):
+        """Updates the cache of banned IP addresses"""
 
-post_save.connect(refresh_banned_ips, sender=BannedIP)
-post_delete.connect(refresh_banned_ips, sender=BannedIP)
+        log.debug('Updating banned IP cache')
+        cache.set('_tracking_banned_ips',
+            [b.ip_address for b in BannedIP.objects.all()],
+            3600)
+
+    post_save.connect(refresh_untracked_user_agents, sender=UntrackedUserAgent)
+    post_delete.connect(refresh_untracked_user_agents, sender=UntrackedUserAgent)
+
+    post_save.connect(refresh_banned_ips, sender=BannedIP)
+    post_delete.connect(refresh_banned_ips, sender=BannedIP)
